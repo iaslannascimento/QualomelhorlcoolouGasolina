a7ce30161af42d6d2a0d066eb6168f06597bf5da
iaslannascimento <iaslan.nascimento@hotmail.com>
2019-01-28 17:46:42 -0300

    Final

--- app/src/main/java/com/example/user/qualomelhorlcoolougasolina/MainActivity.java
+++ app/src/main/java/com/example/user/qualomelhorlcoolougasolina/MainActivity.java
@@ -20,26 +20,45 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-
+    //método chamado pelo botão
     public void calcularPreco(View View){
         //recuperando valores
         String precoAlcool = vAlcool.getText().toString();
-        String precoGasolina = vGasolina.getText();toString();
+        String precoGasolina = vGasolina.getText().toString();
 
         //validação
         boolean validacao = validacao(precoAlcool,precoGasolina);
         if(validacao){
-
+            this.calcularMelhorPreco(precoAlcool,precoGasolina);
         }else{
             result.setText("Preencha os campos primeiro");
         }
     }
+
+
+    //método para calcular o melhor preço entre gasolina e alcool
+    public void calcularMelhorPreco(String precoAlcool, String precoGasolina){
+        //converção de valores
+        Double pAlcool = Double.parseDouble(precoAlcool);
+        Double pGasolina = Double.parseDouble(precoGasolina);
+        //calculo utilizado melhor(alcool/gasolina se o resultado for maior que 0.7 é melhor usar gasolina se não alcool
+        double x;
+        x=(pAlcool/pGasolina);
+        if(x >= 0.7){
+            result.setText("É melhor usar Gasolina");
+        }else {
+            result.setText("É melhor usar Álcool");
+        }
+
+    }
+
+    //método de validação
     public boolean validacao(String pAlcool, String pGasolina){
          boolean campos = true;
 
-         if(pAlcool == null || pAlcool == equals("")){
+         if(pAlcool == null || pAlcool.equals("")){
              campos = false;
-         }else if(pGasolina == null || pGasolina == equals("")){
+         }else if(pGasolina == null || pGasolina.equals("")){
              campos = false;
          }
          return campos;
